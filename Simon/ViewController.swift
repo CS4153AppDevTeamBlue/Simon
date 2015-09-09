e890bf15b66518d9128b3a4e61a85a1e414f636e
Kishotta <connoreaves@Connor-Eavess-iMac.local>
2015-09-09 04:00:59 -0500

    Changed naming and formatting for consistency and to somewhat adhere to raywenderlich's Swift style guide

--- Simon/ViewController.swift
+++ Simon/ViewController.swift
@@ -11,118 +11,169 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    
+    // ===== Public variables and constants ===== //
+    
+    
     var ButtonPattern = [String]()
-    var indexArray: [UInt32] = []
-    var gameBeingPlayed = false;
+    var IndexArray = [UInt32]()
+    var GameIsPlaying = false;
     
     var InputIndex = 0
     var Score = 0
     var HighScore = 0
     
-    var currentLength = 1;
+    let InitialPatternLength = 1;
     
-    var audioQueue = AVQueuePlayer()
+    let PatternAudioPlayer = AVQueuePlayer()
     
+    
+    // ===== Outlet declarations ===== //
     
     
     // This is for debugging/prototyping purposes only and should be removed in
     // the final build.
     @IBOutlet weak var ButtonPatternLabel: UILabel!
+    
     @IBOutlet weak var ScoreLabel: UILabel!
     @IBOutlet weak var HighScoreLabel: UILabel!
-    @IBOutlet weak var startNewGame: UIButton!
+    
+    @IBOutlet weak var StartNewGameButton: UIButton!
+    
+    
+    // ===== UIViewController overrides ===== //
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        // Make our initial button pattern
-        //ResetButtonPattern()
-        
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // ===== Start New Game button (Entry point) ===== //
+    
+    
     @IBAction func StartGameBuittonPress(sender: AnyObject) {
-        gameBeingPlayed = true
-        startNewGame.hidden = true
+        
+        GameIsPlaying = true
+        
+        if let newGameButton = StartNewGameButton {
+            StartNewGameButton.hidden = true
+        }
+        
         let seconds = 1.0
         let delay = seconds * Double(NSEC_PER_SEC)  // nanoseconds per seconds
         let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        
         dispatch_after(dispatchTime, dispatch_get_main_queue(), {
-            self.ResetButtonPattern()
-        })
+                self.InitNewButtonPattern()
+            }
+        )
+        
     }
     
+    
+    // ===== Button press handling ===== //
+    
+    
     @IBAction func RedButtonPress(sender: AnyObject) {
-        if(gameBeingPlayed) {
-        audioQueue.removeAllItems()
-        ButtonPressed(0)
-        audioQueue.play()
         
-        HandleInputPattern(0)
+        if GameIsPlaying {
+            PatternAudioPlayer.removeAllItems()
+            AddButtonAudioToPatternPlayer(0)
+            PatternAudioPlayer.play()
+        
+            HandleInputPattern(0)
         }
         
     }
     
     @IBAction func GreenButtonPress(sender: AnyObject) {
-        if(gameBeingPlayed) {
-        ButtonPressed(1)
-        HandleInputPattern(1)
-        audioQueue.play()
+        
+        if GameIsPlaying {
+            PatternAudioPlayer.removeAllItems()
+            AddButtonAudioToPatternPlayer(1)
+            PatternAudioPlayer.play()
+            
+            HandleInputPattern(1)
         }
     }
     
     @IBAction func YellowButtonPress(sender: AnyObject) {
-        if(gameBeingPlayed) {
-        ButtonPressed(2)
-        HandleInputPattern(2)
-        audioQueue.play()
-        }
-    }
-    
-    @IBAction func BlueButtonPress(sender: AnyObject) {
-        if(gameBeingPlayed) {
-        ButtonPressed(3)
-        HandleInputPattern(3)
-        audioQueue.play()
-        }
-    }
-    
-    func ButtonPressed(inputIndex: UInt32)
-    {
-        switch(inputIndex)
-        {
-        case 0 :
-                    let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Red", withExtension: "wav")!
-                    LoadButtonSound(buttonSoundFile)
-                    break
-        case 1 :
-                    let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Green", withExtension: "wav")!
-                    LoadButtonSound(buttonSoundFile)
-                    break
-        case 2 :
-                    let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Yellow", withExtension: "wav")!
-                    LoadButtonSound(buttonSoundFile)
-                    break
-        case 3 :
-                    let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Blue", withExtension: "wav")!
-                    LoadButtonSound(buttonSoundFile)
-                    break
-        default :
-                    // may need to play error noise
-                    break
+        
+        if GameIsPlaying {
+            PatternAudioPlayer.removeAllItems()
+            AddButtonAudioToPatternPlayer(2)
+            PatternAudioPlayer.play()
+            
+            HandleInputPattern(2)
         }
         
     }
     
-    func LoadButtonSound(buttonSoundFile: NSURL)
-    {
-    
-        let audioItem = AVPlayerItem(URL: buttonSoundFile)
-        audioQueue.insertItem(audioItem, afterItem: nil)
-    
+    @IBAction func BlueButtonPress(sender: AnyObject) {
+        
+        if GameIsPlaying {
+            PatternAudioPlayer.removeAllItems()
+            AddButtonAudioToPatternPlayer(3)
+            PatternAudioPlayer.play()
+            
+            HandleInputPattern(3)
+        }
+        
     }
     
+    
+    // =====
+    
+    
+    func AddButtonAudioToPatternPlayer(inputIndex: UInt32) {
+        
+        switch(inputIndex) {
+            
+            case 0 :
+                
+                let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Red", withExtension: "wav")!
+                let audioItem = AVPlayerItem(URL: buttonSoundFile)
+                PatternAudioPlayer.insertItem(audioItem, afterItem: nil)
+                break
+            
+            case 1 :
+                
+                let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Green", withExtension: "wav")!
+                let audioItem = AVPlayerItem(URL: buttonSoundFile)
+                PatternAudioPlayer.insertItem(audioItem, afterItem: nil)
+                break
+            
+            case 2 :
+                
+                let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Yellow", withExtension: "wav")!
+                let audioItem = AVPlayerItem(URL: buttonSoundFile)
+                PatternAudioPlayer.insertItem(audioItem, afterItem: nil)
+                break
+            
+            case 3 :
+                
+                let buttonSoundFile =  NSBundle.mainBundle().URLForResource("Simon_Blue", withExtension: "wav")!
+                let audioItem = AVPlayerItem(URL: buttonSoundFile)
+                PatternAudioPlayer.insertItem(audioItem, afterItem: nil)
+                break
+            
+            default :
+                
+                // may need to play error noise
+                break
+        }
+        
+    }
+    
+    
+    // =====
     
     
     func HandleInputPattern(input: UInt32) {
@@ -132,35 +183,39 @@ class ViewController: UIViewController {
             
             // If we are at the end of the pattern
             if InputIndex == ButtonPattern.count - 1 {
+                
+                IncreaseScore(1)
+                
                 let seconds = 1.5
                 let delay = seconds * Double(NSEC_PER_SEC)  // nanoseconds per seconds
                 let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
                 
                 dispatch_after(dispatchTime, dispatch_get_main_queue(), {
-                    //Increase Score
-                    self.increaseScore()
                     
-                    // here code perfomed with delay
-                    // Reset input index
-                    self.InputIndex = 0
+                        // here code perfomed with delay
+                        // Reset input index
+                        self.InputIndex = 0
                     
-                    // Extend ButtonPattern
-                    let newInt = UInt32(arc4random_uniform(4))
-                    self.indexArray.append(newInt)
-                    self.ButtonPattern.append(newInt.description)
+                        // Extend ButtonPattern
+                        let newInt = UInt32(arc4random_uniform(4))
+                        self.IndexArray.append(newInt)
+                        self.ButtonPattern.append(newInt.description)
                     
-                    // update the label
-                    self.ButtonPatternLabel.text? += newInt.description
+                        // update the label
+                        self.ButtonPatternLabel.text? += newInt.description
                     
-                    // Animate Pattern
-                    self.PlayButtonPattern(&self.indexArray)
+                        // Animate Pattern
+                        self.PlayButtonPattern(&self.IndexArray)
                     
-                })
+                    }
+                )
                 
                 // If we are not at the end of the pattern
             } else {
+                
                 // Move on to the next button in the pattern
                 InputIndex++
+                
             }
             
             // If we hit the wrong button
@@ -173,8 +228,8 @@ class ViewController: UIViewController {
                     print("default")
                     
                     //Reset score and Button Pattern
-                    self.gameBeingPlayed = false
-                    self.startNewGame.hidden = false
+                    self.GameIsPlaying = false
+                    self.StartNewGameButton.hidden = false
                     self.Score = 0
                     self.ScoreLabel.text = "0"
                     //self.ResetButtonPattern()
@@ -190,58 +245,58 @@ class ViewController: UIViewController {
         }
     }
     
-    func ResetButtonPattern() {
+    
+    
+    
+    
+    func InitNewButtonPattern() {
+        
         // Reset our input and clear the pattern
         InputIndex = 0
         ButtonPatternLabel.text = ""
         ButtonPattern.removeAll(keepCapacity: false)
-        indexArray.removeAll(keepCapacity: false)
+        IndexArray.removeAll(keepCapacity: false)
         
         Score = 0
         ScoreLabel.text = "0"
         HighScoreLabel.text = HighScore.description
         
-        
         // Throw a for loop right here to have the pattern start at any given
         // length.
-        for(var i = 0; i < currentLength; i++)
-        {
-            indexArray.append(arc4random_uniform(4))
-            ButtonPattern.append(indexArray[i].description) //not sure what this is going to do so left it in --K.Angel
+        for var i = 0; i < InitialPatternLength; i++ {
+            
+            IndexArray.append(arc4random_uniform(4))
+            ButtonPattern.append(IndexArray[i].description)
             
             // update the label
             ButtonPatternLabel.text? += ButtonPattern[i]
             
         }
-        PlayButtonPattern(&indexArray)
         
+        PlayButtonPattern(&IndexArray)
         
     }
     
-    func increaseScore( ) {
-        Score++
+    func IncreaseScore(points: Int) {
+        
+        Score += points
         ScoreLabel.text = Score.description
+        
         if Score > HighScore {
             HighScore++
             HighScoreLabel.text = HighScore.description
         }
+        
     }
     
-    func PlayButtonPattern(inout someArray: [UInt32])
-    {
-        for (var i = 0; i < someArray.count; i++)
-        {
-            ButtonPressed(someArray[i])
+    func PlayButtonPattern(inout someArray: [UInt32]) {
+        
+        for var i = 0; i < someArray.count; i++ {
+            AddButtonAudioToPatternPlayer(someArray[i])
         }
         
-        audioQueue.play() // plays queue but we may want to refactor this so we can make buttons light up
+        PatternAudioPlayer.play() // plays queue but we may want to refactor this so we can make buttons light up
     
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
 }
